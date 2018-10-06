0ecfcf7ef6759aa1ee6f88e6ee04193e261ca091
Anton Burtsev <aburtsev@flux.utah.edu>
2018-10-06 13:25:19 -0700

    Add HW1 OS Interface and Shell

---
+++ hw/main.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+int
+main(void)
+{
+  /* Insert your code here */
+  exit(0);
+}
+
+
