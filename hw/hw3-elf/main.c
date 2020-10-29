92fa06d2e046817e2ef913ef0a4e78e3888a4097
Anton Burtsev <aburtsev@uci.edu>
2020-10-28 20:51:05 -0400

    HW3 ELF

---
+++ hw/hw3-elf/main.c
@@ -0,0 +1,78 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+#include <sys/mman.h>
+
+
+#include <sys/types.h>
+#include <unistd.h>
+
+// Format of an ELF executable file
+
+#define ELF_MAGIC 0x464C457FU  // "\x7FELF" in little endian
+
+// File header
+struct elfhdr {
+  unsigned int magic;  // must equal ELF_MAGIC
+  unsigned char elf[12];
+  unsigned short type;
+  unsigned short machine;
+  unsigned int version;
+  unsigned int entry;
+  unsigned int phoff;
+  unsigned int shoff;
+  unsigned int flags;
+  unsigned short ehsize;
+  unsigned short phentsize;
+  unsigned short phnum;
+  unsigned short shentsize;
+  unsigned short shnum;
+  unsigned short shstrndx;
+};
+
+// Program section header
+struct proghdr {
+  unsigned int type;
+  unsigned int off;
+  unsigned int vaddr;
+  unsigned int paddr;
+  unsigned int filesz;
+  unsigned int memsz;
+  unsigned int flags;
+  unsigned int align;
+};
+
+// Values for Proghdr type
+#define ELF_PROG_LOAD           1
+
+// Flag bits for Proghdr flags
+#define ELF_PROG_FLAG_EXEC      1
+#define ELF_PROG_FLAG_WRITE     2
+#define ELF_PROG_FLAG_READ      4
+
+int main(int argc, char* argv[]) {
+    struct elfhdr elf;
+    struct proghdr ph;
+    int (*sum)(int a, int b);
+    void *entry = NULL;
+    int ret; 
+
+    /* Add your ELF loading code here */
+
+    if (entry != NULL) {
+        sum = entry; 
+        ret = sum(1, 2);
+        printf("sum:%d\n", ret); 
+    };
+
+
+}
+
+
