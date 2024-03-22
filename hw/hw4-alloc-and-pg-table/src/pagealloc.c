590305ce0bbf21a8666f73246f7d648ae55821d0
jerry <jerryzhang19257@gmail.com>
2024-03-22 17:16:09 -0600

    changes hw4

---
+++ hw/hw4-alloc-and-pg-table/src/pagealloc.c
@@ -0,0 +1,33 @@
+#include "mmu.h" // optional
+
+#define MAX_PAGES (1 << 20) 
+#define NIL 0xFFFFFFFF
+
+struct page {
+    unsigned int next;
+};
+
+struct free_pages {
+    unsigned int head;
+    struct page pages[MAX_PAGES];
+};
+
+struct free_pages page_allocator = {
+  .head = NIL,
+};
+
+unsigned int page_alloc() 
+{
+  //TODO
+}
+
+void page_free(unsigned int pa) 
+{
+  //TODO
+}
+
+void mmap(pte_t* pgdir, unsigned int va, unsigned int pa, unsigned int flag) 
+{
+  //TODO
+}
+
